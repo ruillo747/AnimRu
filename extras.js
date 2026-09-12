4ce39284ad82f7d54ce65965d25af966c4ea332d
ruillo747 <nozirovruillo@gmail.com>
2026-09-12 22:47:13 +0300

    Обновление версии модулей до v20

--- extras.js
+++ extras.js
@@ -91,7 +91,7 @@
     ['polish.js', 'kodik-net.js', 'mobilefix.js', 'offline.js', 'schedule.js', 'kodik-browse.js', 'catalog.js'].forEach(function (file) {
       if (document.querySelector('script[src*="' + file + '"]')) return;
       var script = document.createElement('script');
-      script.src = file + '?v=19';
+      script.src = file + '?v=20';
       script.defer = true;
       document.body.appendChild(script);
     });
